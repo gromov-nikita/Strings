24ef1b48ec3c5cb12efc9221e62c29f82396413a
gromov-nikita <hromay.n999@gmail.com>
2021-02-15 19:00:54 +0300

    1

--- 4)/src/Receipt.java
+++ 4)/src/Receipt.java
@@ -1,19 +1,53 @@
+/*
+Exercise 4: (3) Modify Receipt.java so that the widths are all controlled by a single set of
+constant values. The goal is to allow you to easily change a width by changing a single value
+in one place.
+ */
 import java.util.*;
 public class Receipt {
     private double total = 0;
+    private static String lengthItem = "%-16s";
+    private static String lengthQty = "%6s";
+    private static String lengthPrice = "%10s";
+    private static String lengthDoublePrice = "%10.2f";
+    private static String lengthName = "%-16.15s";
+    private static final int indent = 15;
     private Formatter f = new Formatter(System.out);
+    public static String getLengthItem() {
+        return lengthItem;
+    }
+    public static void setLengthItem(String lengthItem) {
+        Receipt.lengthItem = lengthItem;
+    }
+
+    public static String getLengthQty() {
+        return lengthQty;
+    }
+
+    public static void setLengthQty(String lengthQty) {
+        Receipt.lengthQty = lengthQty;
+    }
+
+    public static String getLengthPrice() {
+        return lengthPrice;
+    }
+    public static void setLengthPrice(String lengthPrice) {
+        Receipt.lengthPrice = lengthPrice;
+    }
+
+
     public void printTitle() {
-        f.format("%-15s %5s %10s\n", "Item", "Qty", "Price");
-        f.format("%-15s %5s %10s\n", "----", "---", "-----");
+        f.format(lengthItem + lengthQty + lengthPrice + "\n", "Item", "Qty", "Price");
+        f.format(lengthItem + lengthQty + lengthPrice +"\n", "----", "---", "-----");
     }
     public void print(String name, int qty, double price) {
-        f.format("%-15.15s %5d %10.2f\n", name, qty, price);
+        f.format(lengthName + lengthQty + lengthDoublePrice + "\n", name, qty, price);
         total += price;
     }
     public void printTotal() {
-        f.format("%-15s %5s %10.2f\n", "Tax", "", total*0.06);
-        f.format("%-15s %5s %10s\n", "", "", "-----");
-        f.format("%-15s %5s %10.2f\n", "Total", "",
+        f.format(lengthItem + lengthQty + lengthDoublePrice + "\n", "Tax", "", total*0.06);
+        f.format(lengthItem + lengthQty + lengthPrice + "\n", "", "", "-----");
+        f.format(lengthItem + lengthQty + lengthDoublePrice + "\n", "Total", "",
                 total * 1.06);
     }
     public static void main(String[] args) {
